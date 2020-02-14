53966cc6907b9c450b0cad833671f5d3bf459377
daimessdn <daimessdn@gmail.com>
2020-02-14 18:17:26 +0700

    registered packages on index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const serverless = require("serverless-http");
 
 const app = express();
 
@@ -9,3 +10,6 @@ app.use(express.static('public'));
 app.listen(PORT, () => {
 	console.log(`the server is listening in http://127.0.0.1:${PORT} on your browser.`);
 });
+
+module.exports = app;
+module.exports.handler = serverless(app);
